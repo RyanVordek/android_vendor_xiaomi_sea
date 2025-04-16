533e36ffcc42c0220084d47d500f170ad575b8f3
RyanVordek <drsaluismiguel@gmail.com>
2025-04-16 16:03:58 -0300

    sea: initial

---
+++ Android.mk
@@ -0,0 +1,21 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),sea)
+
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,f2aa62231602a1fdb1bb4c94301bbbd471c7f409)
+$(call add-radio-file-sha1-checked,radio/gz.img,86e56a8f40a9f73ad2dfd866397e4c49331e2000)
+$(call add-radio-file-sha1-checked,radio/lk.img,721066cd540260ddd3277975b4a8d0c4daae59d2)
+$(call add-radio-file-sha1-checked,radio/logo.img,3d380544b3e75d00abd940c1ad4a4905f510fcc8)
+$(call add-radio-file-sha1-checked,radio/md1img.img,c6600c743d1cac6bcfc69eb64b58819c119add11)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,d941b807c51b85e41c21abc3409f418301e27934)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,ad165d5cece29ca4955b560e47d527408e2f2f84)
+$(call add-radio-file-sha1-checked,radio/scp.img,e8ee85efe4e212a040614949a1472befc665ca73)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,b6d58d1670ffdd2ae91d0b150e4f7cbe2f913f88)
+$(call add-radio-file-sha1-checked,radio/sspm.img,acc3898e7dcab9b583b768df9609939740ec4ae8)
+$(call add-radio-file-sha1-checked,radio/tee.img,bd8e2cbbf2acab607ffaefe5887f3493c61a1a84)
+
+endif
